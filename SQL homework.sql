2402904322ffb089a4a245ff0adbf6e3de09eb44
Jordan Lyons <JordanLyons@gmail.com>
2016-09-24 15:22:02 -0600

    Added my independent query project

---
+++ SQL homework.sql
@@ -0,0 +1,18 @@
+﻿SELECT Customers.Companyname
+, Orders.OrderDate
+, Orders.OrderID
+, Products.ProductName
+, Order_Details.Quantity
+, Order_Details.UnitPrice * Order_Details.Quantity AS ExtendedPrice
+, Orders.OrderDate
+FROM Customers
+INNER JOIN Orders
+ON Customers.CustomerID = Orders.CustomerID
+INNER JOIN Order_Details
+ON Orders.OrderID = Order_Details.OrderID
+INNER JOIN Products
+ON Products.ProductID = Order_Details.ProductID
+INNER JOIN Categories
+ON Categories.CategoryID = Products.CategoryID
+WHERE EXTRACT(YEAR FROM Orders.OrderDate) = 1996
+ORDER BY ExtendedPrice DESC
